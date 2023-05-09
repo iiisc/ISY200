4c34f013d49e2672577ad02d07d462abf063fe5c
iiisc <63256807+iiisc@users.noreply.github.com>
2023-05-10 00:01:30 +0200

    checkspeed implemented

--- Laboration 2 - Vindrutetorkare/Laboration2_svep.cpp
+++ Laboration 2 - Vindrutetorkare/Laboration2_svep.cpp
@@ -6,24 +6,32 @@ PwmOut servo(p5);
 PwmOut led(p5);
 AnalogOut pot(p15);
 
-void svep(int speed) {
+float checkSpeed(){
+    return 500-(400*pot.read());
+}
+
+void svep() {
     //Om speed = 100 kommer rörelse ett håll ta 1sekund
     servo.period(0.020);
     float start = 0.0009;
     float slut = 0.0021;
     int cycles = 10; //Styr "upplösningen"
     float cycle_time = (slut-start)/cycles;
+    
+    float speed = checkSpeed();
 
         for (float i = start; i < slut; i += cycle_time) {
             //Hundra varv i denna loop tar vår servo från min > max
             servo.pulsewidth(i);
-            printf("PwmOut: %f, >>Max \n", i);
+            float speed = checkSpeed();
+            printf("PwmOut: %f, Speed: %f, >>Max \n", i, speed);
             wait_ms(speed);
         }
         for (float i = slut; i > start; i -= cycle_time) {
             //Hundra varv i denna loop tar vår servo från max > min
             servo.pulsewidth(i);
-            printf("PwmOut: %f, >>Min\n", i); 
+            float speed = checkSpeed();
+            printf("PwmOut: %f, Speed: %f, >>Min\n", i, speed); 
             wait_ms(speed);
         }
 }
@@ -31,14 +39,8 @@ void svep(int speed) {
 int main() {
     while(1) {
         //servo_thread.start(svep(100));
-        //svep(100);
-        //printf("Cycle done\n");
-        float speed = 50 + (50/pot.read());         
-        if (pot.read() < 0.1) {
-            speed = 500; 
-        }
-        printf("Speed: %.3f\n", speed);
-        
-        wait(0.2);
+        svep();
+        printf("Cycle done\n");
+        wait(4);
     }
 }
