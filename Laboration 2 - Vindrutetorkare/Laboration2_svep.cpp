92fa73bb35e5bdef8cabbee70a0336cbaf5f0edf
iiisc <63256807+iiisc@users.noreply.github.com>
2023-05-09 15:46:28 +0200

    Svep?

---
+++ Laboration 2 - Vindrutetorkare/Laboration2_svep.cpp
@@ -0,0 +1,35 @@
+#include "mbed.h"
+
+PwmOut servo(p5);
+//Thread servo_thread;
+
+void svep(int speed) {
+    //Om speed = 10 kommer rörelse ett håll ta 1sekund
+    servo.period(0.020);
+    float start = 0.0009;
+    float slut = 0.0021;
+    int cycles = 10;
+    float cycle_time = (slut-start)/cycles;
+
+        for (float i = start; i < slut; i += cycle_time) {
+            //Hundra varv i denna loop tar vår servo från min > max
+            servo.pulsewidth(i);
+            printf("PwmOut: %f, >>Max \n", i);
+            wait_ms(speed);
+        }
+        for (float i = slut; i > start; i -= cycle_time) {
+            //Hundra varv i denna loop tar vår servo från max > min
+            servo.pulsewidth(i);
+            printf("PwmOut: %f, >>Min\n", i); 
+            wait_ms(speed);
+        }
+}
+
+int main() {
+    while(1) {
+        //servo_thread.start(svep(100));
+        svep(100);
+        printf("Cycle done\n");
+        wait(5);
+    }
+}
