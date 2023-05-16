caaef36c80033d768e02e75cc4f2f12bbf52b9e7
Carl Lindbom <63256807+iiisc@users.noreply.github.com>
2023-05-16 15:52:36 +0200

    Update Laboration3_slave.cpp

--- Laboration 3 - Seriell kommunkation/Laboration3_slave.cpp
+++ Laboration 3 - Seriell kommunkation/Laboration3_slave.cpp
@@ -1,16 +1,16 @@
 #include "mbed.h"
+#include <chrono>
 
-SPISlave slave(p11, p12, p13, p14); // mosi, miso, sclk, ssel
-DigitalOutput led1(LED1);
-DigitalOutput led2(LED2);
-DigitalOutput led3(LED3);
-DigitalOutput led4(LED4);
-
+SPISlave slave(p11, p12, p13, p14); // mosi, miso, sclk ssel
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
+DigitalOut led4(LED4);
+unsigned char input;
 
 void lamps(int speed){
     // Speed är initialt ett värde mellan 0 och 100. 
-    wait_time = 300-(speed*2) //Som snabbast 100ms, som långsamast 300ms.
-
+    int wait_time = 400-(speed*3.95); //Som snabbast 100ms, som långsamast 300ms.
     led1 = 1;
     led2 = 0;
     led3 = 0;
@@ -35,19 +35,18 @@ void lamps(int speed){
 
 int main()
 {
-    slave.format(8.0);
+
+    slave.format(8,0);
     slave.frequency(1000000);
-    slave.reply(0x01); // Första svaret
-    
-    unsigned char output = 0x01; // Det ständiga svaret. En etta. 
     led1 = 0; led2 = 0; led3 = 0; led4 = 0; //Släck alla lampor
 
     while (1) {
+        
         if (slave.receive()) {
-            int input = slave.read(); // Läs från master. Kommer vara ett värde mellan 0 och 100
-            slave.reply(output); // Skicka tillbaka till master
-            lamps(input);
-            ThisThread::sleep_for(chrono::milliseconds(50));
+        input = slave.read(); // Läs från master
+        slave.reply(0x01);
+        lamps(input);
         }
+        ThisThread::sleep_for(chrono::milliseconds(50));
     }
 }
