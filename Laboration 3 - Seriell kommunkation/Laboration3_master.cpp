8cafb4025acbd5c4103c6ebe29e319d30209a430
iiisc <63256807+iiisc@users.noreply.github.com>
2023-05-16 09:44:01 +0200

    Slave unt master

--- Laboration 3 - Seriell kommunkation/Laboration3_master.cpp
+++ Laboration 3 - Seriell kommunkation/Laboration3_master.cpp
@@ -5,15 +5,10 @@ using namespace std;
 
 SPI ser_port(p11, p12, p13); // mosi, miso, sclk
 DigitalOut cs(p14); // Detta är slave-select
-char switch_word; //word we will send
-
 AnalogIn pot(p19);
 
+unsigned char switch_word; //word we will send
 
-int checkSpeed(){
-    //Läser potentiometer. 1000 vid pot = 0, 300 vid pot = 1. 
-    return 400-(300*pot.read());
-}
 
 int main() {
     ser_port.format(8,0); // Setup the SPI for 8 bit data, Mode 0 operation
@@ -21,8 +16,8 @@ int main() {
     switch_word=0x01; //Skickar en etta
 
     while (1){
-        switch_word = ser_port.write(switch_word); //send switch_word and update switch_word with recieved value
-        //ThisThread::sleep_for(chrono::microseconds(50))
-        ThisThread::sleep_for(chrono::milliseconds(checkSpeed()));
+        ser_port.write(switch_word); //Skicka switch_word
+        switch_word = pot.read()*100; //Uppdatera switch_word till ett värde mellan 0 och 100. 
+        ThisThread::sleep_for(chrono::milliseconds(50));
     }
 }
--- Laboration 3 - Seriell kommunkation/Laboration3_slave.cpp
+++ Laboration 3 - Seriell kommunkation/Laboration3_slave.cpp
@@ -1,38 +1,53 @@
 #include "mbed.h"
 
-SPISlave device(p11, p12, p13, p14); // mosi, miso, sclk, ssel
+SPISlave slave(p11, p12, p13, p14); // mosi, miso, sclk, ssel
 DigitalOutput led1(LED1);
 DigitalOutput led2(LED2);
 DigitalOutput led3(LED3);
+DigitalOutput led4(LED4);
 
+
+void lamps(int speed){
+    // Speed är initialt ett värde mellan 0 och 100. 
+    wait_time = 300-(speed*2) //Som snabbast 100ms, som långsamast 300ms.
+
+    led1 = 1;
+    led2 = 0;
+    led3 = 0;
+    led4 = 0;
+    ThisThread::sleep_for(chrono::milliseconds(wait_time));
+    led1 = 0;
+    led2 = 1;
+    led3 = 0;
+    led4 = 0;
+    ThisThread::sleep_for(chrono::milliseconds(wait_time));
+    led1 = 0;
+    led2 = 0;
+    led3 = 1;
+    led4 = 0;
+    ThisThread::sleep_for(chrono::milliseconds(wait_time));
+    led1 = 0;
+    led2 = 0;
+    led3 = 0;
+    led4 = 1;
+    ThisThread::sleep_for(chrono::milliseconds(wait_time));
+}
 
 int main()
 {
-    device.format(8.0);
-    device.frequency(1000000);
-    device.reply(0x00);              // Prime SPI with first reply
+    slave.format(8.0);
+    slave.frequency(1000000);
+    slave.reply(0x01); // Första svaret
+    
+    unsigned char output = 0x01; // Det ständiga svaret. En etta. 
+    led1 = 0; led2 = 0; led3 = 0; led4 = 0; //Släck alla lampor
 
     while (1) {
-        if (device.receive()) {
-            int input = device.read();   // Read byte from master
-            input = (input+1) % 3;     // Går från 0 till 2
-            device.reply(v);         // Make this the next reply
-        }
-        if (input == 0) {
-            led1 = 1;
-            led2 = 0;
-            led3 = 0;
-
-        }
-        if (input == 1) {
-            led1 = 0;
-            led2 = 1;
-            led3 = 0;
-        }
-        if (input == 2) {
-            led1 = 0;
-            led2 = 0;
-            led3 = 1;
+        if (slave.receive()) {
+            int input = slave.read(); // Läs från master. Kommer vara ett värde mellan 0 och 100
+            slave.reply(output); // Skicka tillbaka till master
+            lamps(input);
+            ThisThread::sleep_for(chrono::milliseconds(50));
         }
     }
 }
